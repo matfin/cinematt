13c919dccbf22d2c2dc3bf43cbfc22f3a4eba99b
Matt Finucane <matfin@gmail.com>
2017-06-11 19:26:22 +0200

    Function to generate gradients fixed to deliver solid gradients.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -14,7 +14,16 @@ const addBars = (photo_card) => {
 	photo_card.style.backgroundImage = `linear-gradient(to right, ${colours})`;
 };
 
+const addGradients = (photo_card) => {
+	let colours = photo_card.getAttribute('data-colours').split(','),
+		step = 100 / colours.length;
+
+	let gradient = colours.map((colour, index) => `${colour} ${index * step}%, ${colour} ${++index * step}%`).join(',');
+	photo_card.style.backgroundImage = `linear-gradient(to right, ${gradient})`;
+};
+
 const makeBars = () => {
 	let cards = [...document.querySelectorAll('figure.photo-card')];
-	cards.forEach(card => addBars(card));
+	// cards.forEach(card => addBars(card));
+	cards.forEach(addGradients);
 };
