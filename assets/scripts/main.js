62e4e501a8ec5863d4a15f6da51fae74a0055cb5
Matt Finucane <matfin@gmail.com>
2017-06-11 23:20:22 +0200

    Adding JS to load images.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,25 +5,6 @@ if(window.cinematt == null) {
 }
 
 onload = () => {
-	makeBars();	
-};
-
-const addBars = (photo_card) => {
-	let colours = photo_card.getAttribute('data-colours');
-	photo_card.removeAttribute('data-colours');
-	photo_card.style.backgroundImage = `linear-gradient(to right, ${colours})`;
-};
-
-const addGradients = (photo_card) => {
-	let colours = photo_card.getAttribute('data-colours').split(','),
-		step = 100 / colours.length;
-
-	let gradient = colours.map((colour, index) => `${colour} ${index * step}%, ${colour} ${++index * step}%`).join(',');
-	photo_card.style.backgroundImage = `linear-gradient(to right, ${gradient})`;
-};
-
-const makeBars = () => {
-	let cards = [...document.querySelectorAll('figure.photo-card')];
-	// cards.forEach(card => addBars(card));
-	cards.forEach(addGradients);
+	cinematt.utils.makeBars();
+	cinematt.utils.loadThumbnails();	
 };
